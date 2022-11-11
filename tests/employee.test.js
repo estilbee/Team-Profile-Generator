b837b38a8698d4456788259b1719db901ea8076d
estilbee <odaouphars@gmail.com>
2022-11-11 17:55:06 -0500

    added starter code to all files

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,17 +1,2 @@
-// The first class is an `Employee` parent class with the following properties and methods:
+const Employee = require("../lib/employee")
 
-// * `name`
-
-// * `id`
-
-// * `email`
-
-// * `getName()`
-
-// * `getId()`
-
-// * `getEmail()`
-
-// * `getRole()`&mdash;returns `'Employee'`
-
-// The other three classes will extend `Employee`.
