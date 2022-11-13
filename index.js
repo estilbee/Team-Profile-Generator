adcc80abd7a73957004fca565627b591328859e2
estilbee <odaouphars@gmail.com>
2022-11-13 17:35:50 -0500

    added get engineer function, menu  and endProgram functions

--- index.js
+++ index.js
@@ -32,3 +32,57 @@ const getManager = () => {
     ])
 
 }
+getManager();
+
+const getEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the engineer's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the engineer's GitHub name?",
+            name: "GitHub"
+        }
+    ])
+}
+
+const endProgram = () => {
+    console.log("Exiting program.")
+    return;
+}
+
+function menu(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose an option: ",
+            choices: ["Add an engineer", "Add an intern", "Exit"],
+            name: "menu"
+        }
+    ])
+    .then( ({option}) => {
+        switch(option){
+            case "Add an engineer":
+                return getEngineer();
+            case "Add an intern":
+                return getIntern();
+            case "Exit":
+                return endProgram();       
+        }
+    })
+}
+
+menu();
