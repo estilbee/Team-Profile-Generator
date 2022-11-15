815d4cd9163730ee8ec1358d2e275e668c47fb00
estilbee <odaouphars@gmail.com>
2022-11-15 15:55:28 -0500

    added first test and getIntern to index

--- index.js
+++ index.js
@@ -34,10 +34,6 @@ const getManager = () => {
         console.log(answers)
         const {name, id, email, officeNumber} = answers
         employeeList.push(new Manager(name, id, email, officeNumber))
-        console.log(employeeList)
-        console.log(`
-        ----------------------
-        `)
         return menu()
 })
 }
@@ -69,6 +65,38 @@ const getEngineer = () => {
     console.log(answers)
     const {name, id, email, gitHub} = answers
     employeeList.push(new Engineer(name, id, email, gitHub))
+    return menu()
+    
+})
+}
+
+const getIntern = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the intern's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is the intern's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What school did the intern attend?",
+            name: "school"
+        }
+    ])
+   .then(answers => {
+    console.log(answers)
+    const {name, id, email, school} = answers
+    employeeList.push(new Intern(name, id, email, school))
     console.log(employeeList)
     console.log(`
     ----------------------
@@ -87,7 +115,7 @@ function menu(){
     return inquirer.prompt([
         {
             type: "list",
-            message: "Choose an option: ",
+            message: "Build your team: ",
             choices: ["Add a manager", "Add an engineer", "Add an intern", "Exit"],
             name: "menuOption"
         }
--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,16 @@
 const Engineer = require("../lib/engineer")
+
+
+describe("Role test", () => {
+    describe("Get role method - getRole", () => {
+      it("should return 'Engineer' ", () => {
+        // Setup
+        const engineer = new Engineer();
+  
+        // Expect/Execute
+        expect(engineer.getRole()).toEqual("Engineer");
+  
+        // Teardown
+      });
+    });
+  });
